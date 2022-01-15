259f631b9aca291d6498ea15ac20be973863dfe1
Arne Goeteyn <arne@moonchase.be>
2022-01-15 16:59:41 +0100

    build

---
+++ build/precache-manifest.3df16ec7f2bb6ca1cd25c377d69d8fc8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "b84009f1c27785478df601b00d1349b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "edf487821313164ea226",
+    "url": "/static/css/main.5fa40adf.chunk.css"
+  },
+  {
+    "revision": "edf487821313164ea226",
+    "url": "/static/js/main.edf48782.chunk.js"
+  },
+  {
+    "revision": "1b922744246c39b6bc43",
+    "url": "/static/js/runtime~main.1b922744.js"
+  },
+  {
+    "revision": "354e092f0aa2734f8f4c",
+    "url": "/static/js/vendors~main.354e092f.chunk.js"
+  }
+]);
